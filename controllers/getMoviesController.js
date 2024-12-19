cf13aa9be7117c638033e3028ed900e4ee443b38
SKREAMMY <kiran.pillai199@gmail.com>
2024-12-19 16:15:42 +0000

    added headers to mimic real browser request for vue cinemas

--- controllers/getMoviesController.js
+++ controllers/getMoviesController.js
@@ -25,7 +25,14 @@ nodecron.schedule("*/5 * * * * ", () => {
         let vue_movieList = { "data": [] };
         try {
             const vue_url = "https://www.myvue.com";
-            const sessionResponse = await client.get("https://www.myvue.com");
+            const sessionResponse = await client.get("https://www.myvue.com", {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Accept-Language': 'en-US,en;q=0.9',
+                    'Referer': 'https://www.myvue.com/',
+                }
+            });
             console.log("connection established ", sessionResponse.status);
 
             const dataResponse = await client.get("https://www.myvue.com/api/microservice/showings/cinemas/10016/films", {
