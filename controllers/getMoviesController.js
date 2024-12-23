c6ecaea7839cf7e835bc3506c117338bc2b0fcc2
SKREAMMY <kiran.pillai199@gmail.com>
2024-12-23 19:55:43 +0000

    change spawn env to node env for python

--- controllers/getMoviesController.js
+++ controllers/getMoviesController.js
@@ -141,7 +141,7 @@ nodecron.schedule("*/1 * * * * ", () => {
         });
 
 
-        const python = await spawn('python3', ['./scripts/veu-cinemas.py']);
+        const python = await spawn('/home/node/venv/bin/python3', ['./scripts/veu-cinemas.py']);
 
         let chuncks = []
         // sys.stdout.flush()
