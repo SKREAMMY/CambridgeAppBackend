0a2086eee674c74f5431397de5eb9c7872909f15
SKREAMMY <kiran.pillai199@gmail.com>
2024-12-23 14:49:40 +0000

    fetch via axios for movies api

--- controllers/getMoviesController.js
+++ controllers/getMoviesController.js
@@ -5,7 +5,6 @@ const VueModel = require("../models/getVueMovies")
 const { spawn } = require("child_process")
 const { CookieJar } = require("tough-cookie");
 const { wrapper } = require("axios-cookiejar-support");
-const puppeteer = require("puppeteer");
 
 const jar = new CookieJar();
 const client = wrapper(axios.create({ jar }));
@@ -19,51 +18,30 @@ const convertDateTimeFormatForVue = (date) => {
     return formattedDate;
 }
 
-nodecron.schedule("*/3 * * * * ", () => {
+nodecron.schedule("*/1 * * * * ", () => {
 
     async function fetchVueMovies() {
 
         let vue_movieList = { "data": [] };
         try {
-            const browser = await puppeteer.launch({
-                headless: true,
-                args: ['--no-sandbox',
-                    '--disable-setuid-sandbox',
-                    '--disable-dev-shm-usage', // Use /tmp for shared memory
-                    '--disable-background-timer-throttling',
-                    '--disable-renderer-backgrounding',
-                    '--disable-backgrounding-occluded-windows',],
-                executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-                timeout: 60000,
+            const vue_url = "https://www.myvue.com";
+            const sessionResponse = await client.get("https://www.myvue.com", {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                }
             });
+            console.log("connection established ", sessionResponse.status);
 
-            const page = await browser.newPage();
+            const dataResponse = await client.get("https://www.myvue.com/api/microservice/showings/cinemas/10016/films", {
+                headers: {
+                    'minEmbargoLevel': 3,
+                    'includesSession': 'true',
+                    'includeSessionAttributes': 'true'
+                }
+            })
 
-            // Set headers to mimic a real browser
-            await page.setUserAgent(
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-            );
+            const vueServerData = dataResponse.data.result;
 
-            const baseUrl = 'https://www.myvue.com';
-            await page.goto(baseUrl, { waitUntil: 'networkidle2' });
-            console.log('Session established.');
-
-            const cookies = await browser.cookies();
-            // console.log('Cookies:', cookies);
-
-            const apiUrl =
-                'https://www.myvue.com/api/microservice/showings/cinemas/10016/films?minEmbargoLevel=3&includesSession=true&includeSessionAttributes=true';
-
-            await page.goto(apiUrl, { waitUntil: 'networkidle2' });
-
-            const jsonResponse = await page.evaluate(() => {
-                console.log("doc ", document.body.innerText);
-
-                return JSON.parse(document.body.innerText);
-            });
-
-            const fetchedData = jsonResponse;
-            const vueServerData = fetchedData.result;
 
             for (let i = 0; i < vueServerData.length; i++) {
 
@@ -74,22 +52,25 @@ nodecron.schedule("*/3 * * * * ", () => {
                 movie_details["filmUrl"] = vueServerData[i]["filmUrl"];
                 movie_details["posterImageSrc"] = vueServerData[i]["posterImageSrc"];
                 movie_details["sessions"] = {};
+                // console.log("movie details are ", movie_details);
+                // console.log("length of showing group is ", vueServerData[i]["showingGroups"].length);
 
                 for (let j = 0; j < vueServerData[i]["showingGroups"].length; j++) {
                     let movie_sessions = [];
+                    // console.log(`--- vueServerData ${j}---`);
 
                     for (let k = 0; k < vueServerData[i]["showingGroups"][j]["sessions"].length; k++) {
-
                         let movie_session_details = {};
                         movie_details["duration"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["duration"];
                         movie_session_details["startTime"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["startTime"];
                         movie_session_details["endTime"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["endTime"];
                         movie_session_details["Tickets available"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["isSoldOut"];
-                        movie_session_details["bookingUrl"] = baseUrl + vueServerData[i]["showingGroups"][j]["sessions"][k]["bookingUrl"];
+                        movie_session_details["bookingUrl"] = vue_url + vueServerData[i]["showingGroups"][j]["sessions"][k]["bookingUrl"];
                         movie_session_details["screenName"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["screenName"];
                         movie_session_details["dateofShow"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["showTimeWithTimeZone"];
                         movie_session_details["price"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["formattedPrice"];
                         movie_sessions.push(movie_session_details);
+
                     }
                     movie_details["sessions"][convertDateTimeFormatForVue(vueServerData[i]["showingGroups"][j]["date"])] = movie_sessions;
                 }
@@ -98,7 +79,7 @@ nodecron.schedule("*/3 * * * * ", () => {
             }
 
         } catch (error) {
-            console.error('Error:', error.message);
+            console.error('Error:', error.response?.status, error.response?.data, error.response?.headers);
 
         }
 
@@ -116,8 +97,8 @@ nodecron.schedule("*/3 * * * * ", () => {
         vue_movieList["data"].map(async (d, index) => {
 
             await VueModel.create(d).then((response) => {
-                console.log("created movies");
-                // console.log("added ", index, response);
+                // console.log("created movies");
+                console.log("added ", index, response);
 
             }).catch((err) => {
                 console.log("unable to add the data");
