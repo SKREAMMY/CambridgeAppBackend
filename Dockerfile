027ad2747d9df4c7c39c3f33f2e44ecd6cacd460
SKREAMMY <kiran.pillai199@gmail.com>
2024-12-23 17:47:20 +0000

    adding python dependecies to be installed in container

--- Dockerfile
+++ Dockerfile
@@ -19,9 +19,13 @@ FROM node:22.12-alpine
 RUN mkdir -p /home/node/CambridgeAppBackend/node_modules && chown -R node:node /home/node/CambridgeAppBackend
 WORKDIR /home/node/CambridgeAppBackend
 
+
+
 COPY --chown=node:node package*.json ./
 USER node
 RUN npm install
 COPY --chown=node:node . .
+COPY pythonrequirements.txt ./
+RUN pip3 install --no-cache-dir -r requirements.txt
 EXPOSE 5050
 CMD [ "node", "index.js" ]
--- controllers/getMoviesController.js
+++ controllers/getMoviesController.js
@@ -2,102 +2,132 @@
 const nodecron = require("node-cron")
 const axios = require('axios');
 const VueModel = require("../models/getVueMovies")
-// const { spawn } = require("child_process")
-const { CookieJar } = require("tough-cookie");
-const { wrapper } = require("axios-cookiejar-support");
+const { spawn } = require("child_process")
+// const { CookieJar } = require("tough-cookie");
+// const { wrapper } = require("axios-cookiejar-support");
 
-const jar = new CookieJar();
-const client = wrapper(axios.create({ jar }));
+// const jar = new CookieJar();
+// const client = wrapper(axios.create({ jar }));
 
-const convertDateTimeFormatForVue = (date) => {
-    const newdate = new Date(date);
-    const formattedDate = newdate.toLocaleDateString("en-GB", {
-        day: "2-digit",
-        month: "short",
-    });
-    return formattedDate;
-}
+// const convertDateTimeFormatForVue = (date) => {
+//     const newdate = new Date(date);
+//     const formattedDate = newdate.toLocaleDateString("en-GB", {
+//         day: "2-digit",
+//         month: "short",
+//     });
+//     return formattedDate;
+// }
 
 nodecron.schedule("*/1 * * * * ", () => {
 
-    async function fetchVueMovies() {
+    // async function fetchVueMovies() {
 
-        let vue_movieList = { "data": [] };
-        try {
-            const vue_url = "https://www.myvue.com";
-            const sessionResponse = await client.get("https://www.myvue.com", {
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', // Mimic a real browser
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                    'Accept-Encoding': 'gzip, deflate, br',
-                    'Accept-Language': 'en-US,en;q=0.5',
-                    'Connection': 'keep-alive',
-                    'Upgrade-Insecure-Requests': '1',
-                    'Sec-Fetch-Dest': 'document',
-                    'Sec-Fetch-Mode': 'navigate',
-                    'Sec-Fetch-Site': 'none',
-                    'Sec-Fetch-User': '?1',
-                }
-            });
-            console.log("connection established ", sessionResponse.status);
+    //     let vue_movieList = { "data": [] };
+    //     try {
+    //         const vue_url = "https://www.myvue.com";
+    //         const sessionResponse = await client.get("https://www.myvue.com", {
+    //             headers: {
+    //                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', // Mimic a real browser
+    //                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    //                 'Accept-Encoding': 'gzip, deflate, br',
+    //                 'Accept-Language': 'en-US,en;q=0.5',
+    //                 'Connection': 'keep-alive',
+    //                 'Upgrade-Insecure-Requests': '1',
+    //                 'Sec-Fetch-Dest': 'document',
+    //                 'Sec-Fetch-Mode': 'navigate',
+    //                 'Sec-Fetch-Site': 'none',
+    //                 'Sec-Fetch-User': '?1',
+    //             }
+    //         });
+    //         console.log("connection established ", sessionResponse.status);
 
-            const dataResponse = await client.get("https://www.myvue.com/api/microservice/showings/cinemas/10016/films?minEmbargoLevel=3&includesSession=true&includeSessionAttributes=true", {
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', // Mimic a real browser
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                    'Accept-Encoding': 'gzip, deflate, br',
-                    'Accept-Language': 'en-US,en;q=0.5',
-                    'Connection': 'keep-alive',
-                    'Upgrade-Insecure-Requests': '1',
-                    'Sec-Fetch-Dest': 'document',
-                    'Sec-Fetch-Mode': 'navigate',
-                    'Sec-Fetch-Site': 'none',
-                    'Sec-Fetch-User': '?1',
-                }
-            })
+    //         const dataResponse = await client.get("https://www.myvue.com/api/microservice/showings/cinemas/10016/films?minEmbargoLevel=3&includesSession=true&includeSessionAttributes=true", {
+    //             headers: {
+    //                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', // Mimic a real browser
+    //                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    //                 'Accept-Encoding': 'gzip, deflate, br',
+    //                 'Accept-Language': 'en-US,en;q=0.5',
+    //                 'Connection': 'keep-alive',
+    //                 'Upgrade-Insecure-Requests': '1',
+    //                 'Sec-Fetch-Dest': 'document',
+    //                 'Sec-Fetch-Mode': 'navigate',
+    //                 'Sec-Fetch-Site': 'none',
+    //                 'Sec-Fetch-User': '?1',
+    //             }
+    //         })
 
-            const vueServerData = dataResponse.data.result;
+    //         const vueServerData = dataResponse.data.result;
 
 
-            for (let i = 0; i < vueServerData.length; i++) {
+    //         for (let i = 0; i < vueServerData.length; i++) {
 
-                const movie_details = {};
-                movie_details["filmTitle"] = vueServerData[i]["filmTitle"];
-                movie_details["synopsisShort"] = vueServerData[i]["synopsisShort"];
-                movie_details["director"] = vueServerData[i]["director"];
-                movie_details["filmUrl"] = vueServerData[i]["filmUrl"];
-                movie_details["posterImageSrc"] = vueServerData[i]["posterImageSrc"];
-                movie_details["sessions"] = {};
-                // console.log("movie details are ", movie_details);
-                // console.log("length of showing group is ", vueServerData[i]["showingGroups"].length);
+    //             const movie_details = {};
+    //             movie_details["filmTitle"] = vueServerData[i]["filmTitle"];
+    //             movie_details["synopsisShort"] = vueServerData[i]["synopsisShort"];
+    //             movie_details["director"] = vueServerData[i]["director"];
+    //             movie_details["filmUrl"] = vueServerData[i]["filmUrl"];
+    //             movie_details["posterImageSrc"] = vueServerData[i]["posterImageSrc"];
+    //             movie_details["sessions"] = {};
+    //             // console.log("movie details are ", movie_details);
+    //             // console.log("length of showing group is ", vueServerData[i]["showingGroups"].length);
 
-                for (let j = 0; j < vueServerData[i]["showingGroups"].length; j++) {
-                    let movie_sessions = [];
-                    // console.log(`--- vueServerData ${j}---`);
+    //             for (let j = 0; j < vueServerData[i]["showingGroups"].length; j++) {
+    //                 let movie_sessions = [];
+    //                 // console.log(`--- vueServerData ${j}---`);
 
-                    for (let k = 0; k < vueServerData[i]["showingGroups"][j]["sessions"].length; k++) {
-                        let movie_session_details = {};
-                        movie_details["duration"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["duration"];
-                        movie_session_details["startTime"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["startTime"];
-                        movie_session_details["endTime"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["endTime"];
-                        movie_session_details["Tickets available"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["isSoldOut"];
-                        movie_session_details["bookingUrl"] = vue_url + vueServerData[i]["showingGroups"][j]["sessions"][k]["bookingUrl"];
-                        movie_session_details["screenName"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["screenName"];
-                        movie_session_details["dateofShow"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["showTimeWithTimeZone"];
-                        movie_session_details["price"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["formattedPrice"];
-                        movie_sessions.push(movie_session_details);
+    //                 for (let k = 0; k < vueServerData[i]["showingGroups"][j]["sessions"].length; k++) {
+    //                     let movie_session_details = {};
+    //                     movie_details["duration"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["duration"];
+    //                     movie_session_details["startTime"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["startTime"];
+    //                     movie_session_details["endTime"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["endTime"];
+    //                     movie_session_details["Tickets available"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["isSoldOut"];
+    //                     movie_session_details["bookingUrl"] = vue_url + vueServerData[i]["showingGroups"][j]["sessions"][k]["bookingUrl"];
+    //                     movie_session_details["screenName"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["screenName"];
+    //                     movie_session_details["dateofShow"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["showTimeWithTimeZone"];
+    //                     movie_session_details["price"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["formattedPrice"];
+    //                     movie_sessions.push(movie_session_details);
 
-                    }
-                    movie_details["sessions"][convertDateTimeFormatForVue(vueServerData[i]["showingGroups"][j]["date"])] = movie_sessions;
-                }
-                vue_movieList["data"].push(movie_details);
+    //                 }
+    //                 movie_details["sessions"][convertDateTimeFormatForVue(vueServerData[i]["showingGroups"][j]["date"])] = movie_sessions;
+    //             }
+    //             vue_movieList["data"].push(movie_details);
 
-            }
+    //         }
 
-        } catch (error) {
-            console.error('Error:', error.response?.status, error.response?.data, error.response?.headers);
+    //     } catch (error) {
+    //         console.error('Error:', error.response?.status, error.response?.data, error.response?.headers);
 
-        }
+    //     }
+
+    //     await VueModel.collection.drop((err, ok) => {
+    //         if (err) {
+    //             console.log("cant delete vue model");
+
+    //         }
+    //         if (ok) {
+    //             console.log("db deleted for vue");
+
+    //         }
+    //     });
+
+    //     vue_movieList["data"].map(async (d, index) => {
+
+    //         await VueModel.create(d).then((response) => {
+    //             // console.log("created movies");
+    //             console.log("added ", index, response);
+
+    //         }).catch((err) => {
+    //             console.log("unable to add the data");
+    //         });
+
+    //     })
+
+
+    // }
+
+    // fetchVueMovies();
+
+    async function getVueMovies() {
 
         await VueModel.collection.drop((err, ok) => {
             if (err) {
@@ -110,22 +140,78 @@ nodecron.schedule("*/1 * * * * ", () => {
             }
         });
 
-        vue_movieList["data"].map(async (d, index) => {
 
-            await VueModel.create(d).then((response) => {
-                // console.log("created movies");
-                console.log("added ", index, response);
+        const python = await spawn('python', ['./scripts/veu-cinemas.py']);
 
-            }).catch((err) => {
-                console.log("unable to add the data");
-            });
+        let chuncks = []
+        // sys.stdout.flush()
+        python.stdout.on('data', async (data) => {
+
+
+            // console.log(`${data}`);
+            // console.log(`${data}`);
+
+
+
+
+            // let data_received = await JSON.parse(`${data}`);
+            chuncks.push(data);
+
+            // console.log(data_received);
+            // console.log(typeof (data));
+            // console.log(data_received["data"].length, " kp ");
+            // console.log("I got ", data_received["data"]);
+            // console.log("new data is .... ", data_received);
+
+
+            // data_received["data"].map(async (d, index) => {
+
+            //     await VueModel.create(d).then((response) => {
+            //         console.log("created movies");
+            //         console.log("added ", index, " ", d);
+
+            //     }).catch((err) => {
+            //         console.log("unable to add the data");
+            //     });
+
+            // })
 
         })
+
+        python.stderr.on('data', (data) => {
+            console.log(` data for stderr + ${data}`);
+        })
+
+        python.on("end", () => {
+
+
+
+        })
+
+        python.on('close', () => {
+            let data = Buffer.concat(chuncks);
+            let result = JSON.parse(data);
+            console.log("finally data is  ", result);
+            result["data"].map(async (d, index) => {
+
+                await VueModel.create(d).then((response) => {
+                    console.log("created movies");
+                    console.log("added ", index, " ", d);
+
+                }).catch((err) => {
+                    console.log("unable to add the data");
+                });
+
+            })
+            console.log("closed cbn");
+        })
+
 
 
     }
 
-    fetchVueMovies();
+    getVueMovies();
+
 
 })
 
