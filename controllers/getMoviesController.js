1f363ab2882b1b261ccb5d1400bfd6526424604b
SKREAMMY <kiran.pillai199@gmail.com>
2025-01-04 13:54:08 +0000

    using puppeteer and headless browser to fetch movie data

--- controllers/getMoviesController.js
+++ controllers/getMoviesController.js
@@ -1,14 +1,16 @@
 
 const nodecron = require("node-cron")
-const axios = require('axios');
+// const axios = require('axios');
 const VueModel = require("../models/getVueMovies")
-const { spawn } = require("child_process")
-const { CookieJar } = require("tough-cookie");
-const { wrapper } = require("axios-cookiejar-support");
-const cloudscraper = require("cloudscraper");
+// const { spawn } = require("child_process")
+// const { CookieJar } = require("tough-cookie");
+// const { wrapper } = require("axios-cookiejar-support");
+// const cloudscraper = require("cloudscraper");
 
-const jar = new CookieJar();
-const client = wrapper(axios.create({ jar }));
+// const jar = new CookieJar();
+// const client = wrapper(axios.create({ jar }));
+
+const puppeteer = requir("pupeteer");
 
 // const convertDateTimeFormatForVue = (date) => {
 //     const newdate = new Date(date);
@@ -25,62 +27,83 @@ nodecron.schedule("*/1 * * * * ", () => {
 
         let vue_movieList = { "data": [] };
         try {
-            const vue_url = "https://www.myvue.com";
-            const sessionResponse = await cloudscraper.get(vue_url, {
-                headers: {
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                    "Referer": vue_url,
-                    "Origin": vue_url,
-                }
-            });
-            console.log("connection established ", sessionResponse.status);
+            // const vue_url = "https://www.myvue.com";
+            // const sessionResponse = await cloudscraper.get(vue_url, {
+            //     headers: {
+            //         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            //         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            //         "Referer": vue_url,
+            //         "Origin": vue_url,
+            //     }
+            // });
+            // console.log("connection established ", sessionResponse.status);
 
-            const dataResponse = await cloudscraper.get("https://www.myvue.com/api/microservice/showings/cinemas/10016/films?minEmbargoLevel=3&includesSession=true&includeSessionAttributes=true", {
-                headers: {
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-                    "Referer": vue_url,
-                    "Origin": vue_url,
-                }
+            // const dataResponse = await cloudscraper.get("https://www.myvue.com/api/microservice/showings/cinemas/10016/films?minEmbargoLevel=3&includesSession=true&includeSessionAttributes=true", {
+            //     headers: {
+            //         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            //         "Referer": vue_url,
+            //         "Origin": vue_url,
+            //     }
+            // })
+
+            let browser;
+            browser = await puppeteer.launch({
+                headless: true,
+                args: ["--no-sandbox", "--disable-setuid-sandbox"],
             })
 
-            const vueServerData = dataResponse.data.result;
+            const page = await browser.newPage();
+
+            await page.setUserAgent(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+            );
+
+            const url = "https://www.myvue.com/api/microservice/showings/cinemas/10016/films?minEmbargoLevel=3&includesSession=true&includeSessionAttributes=true";
+            console.log("navigating to URL: ", url);
+
+            await page.goto(url, { waitUntil: "networkidle2" });
+            const jsonData = await page.evaluate(() => {
+                return JSON.parse(document.body.innerText);
+            });
+
+            console.log("Data fetched successfully!", jsonData.result.length, "movies found.");
+            // const vueServerData = dataResponse.data.result;
 
 
-            for (let i = 0; i < vueServerData.length; i++) {
+            // for (let i = 0; i < vueServerData.length; i++) {
 
-                const movie_details = {};
-                movie_details["filmTitle"] = vueServerData[i]["filmTitle"];
-                movie_details["synopsisShort"] = vueServerData[i]["synopsisShort"];
-                movie_details["director"] = vueServerData[i]["director"];
-                movie_details["filmUrl"] = vueServerData[i]["filmUrl"];
-                movie_details["posterImageSrc"] = vueServerData[i]["posterImageSrc"];
-                movie_details["sessions"] = {};
-                // console.log("movie details are ", movie_details);
-                // console.log("length of showing group is ", vueServerData[i]["showingGroups"].length);
+            //     const movie_details = {};
+            //     movie_details["filmTitle"] = vueServerData[i]["filmTitle"];
+            //     movie_details["synopsisShort"] = vueServerData[i]["synopsisShort"];
+            //     movie_details["director"] = vueServerData[i]["director"];
+            //     movie_details["filmUrl"] = vueServerData[i]["filmUrl"];
+            //     movie_details["posterImageSrc"] = vueServerData[i]["posterImageSrc"];
+            //     movie_details["sessions"] = {};
+            //     // console.log("movie details are ", movie_details);
+            //     // console.log("length of showing group is ", vueServerData[i]["showingGroups"].length);
 
-                for (let j = 0; j < vueServerData[i]["showingGroups"].length; j++) {
-                    let movie_sessions = [];
-                    // console.log(`--- vueServerData ${j}---`);
+            //     for (let j = 0; j < vueServerData[i]["showingGroups"].length; j++) {
+            //         let movie_sessions = [];
+            //         // console.log(`--- vueServerData ${j}---`);
 
-                    for (let k = 0; k < vueServerData[i]["showingGroups"][j]["sessions"].length; k++) {
-                        let movie_session_details = {};
-                        movie_details["duration"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["duration"];
-                        movie_session_details["startTime"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["startTime"];
-                        movie_session_details["endTime"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["endTime"];
-                        movie_session_details["Tickets available"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["isSoldOut"];
-                        movie_session_details["bookingUrl"] = vue_url + vueServerData[i]["showingGroups"][j]["sessions"][k]["bookingUrl"];
-                        movie_session_details["screenName"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["screenName"];
-                        movie_session_details["dateofShow"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["showTimeWithTimeZone"];
-                        movie_session_details["price"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["formattedPrice"];
-                        movie_sessions.push(movie_session_details);
+            //         for (let k = 0; k < vueServerData[i]["showingGroups"][j]["sessions"].length; k++) {
+            //             let movie_session_details = {};
+            //             movie_details["duration"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["duration"];
+            //             movie_session_details["startTime"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["startTime"];
+            //             movie_session_details["endTime"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["endTime"];
+            //             movie_session_details["Tickets available"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["isSoldOut"];
+            //             movie_session_details["bookingUrl"] = vue_url + vueServerData[i]["showingGroups"][j]["sessions"][k]["bookingUrl"];
+            //             movie_session_details["screenName"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["screenName"];
+            //             movie_session_details["dateofShow"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["showTimeWithTimeZone"];
+            //             movie_session_details["price"] = vueServerData[i]["showingGroups"][j]["sessions"][k]["formattedPrice"];
+            //             movie_sessions.push(movie_session_details);
 
-                    }
-                    movie_details["sessions"][convertDateTimeFormatForVue(vueServerData[i]["showingGroups"][j]["date"])] = movie_sessions;
-                }
-                vue_movieList["data"].push(movie_details);
+            //         }
+            //         movie_details["sessions"][convertDateTimeFormatForVue(vueServerData[i]["showingGroups"][j]["date"])] = movie_sessions;
+            //     }
+            //     vue_movieList["data"].push(movie_details);
 
-            }
+            // }
 
         } catch (error) {
             console.error('Error:', error.response?.status, error.response?.data, error.response?.headers);
